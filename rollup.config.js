ac7b8f9046b128867d2c4b7214eb0d79bfb8b4ab
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-24 12:49:56 -0500

    Build stuff.

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+
+const datestr = 'Dec 21, 2024'
+const release = false;
+
+const nodeenv = (release ? 'production' : 'development')
+const tersopt = { format: { ascii_only:true } };
+const tersplugin = (release ? terser(tersopt) : null);
+
+export default {
+    input: 'buildjs/main.js',
+    output: {
+        file: 'js/bundle.js',
+        name: 'bundle',
+        format: 'iife'
+    },
+    plugins: [
+        replace({
+            'preventAssignment': true,
+            'process.env.NODE_ENV': JSON.stringify(nodeenv),
+            '__VISIZORKDATE__': datestr,
+        }),
+        commonjs(),
+        nodeResolve(),
+        tersplugin,
+    ]
+}
+
