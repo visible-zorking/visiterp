2890ecbe68cf827651055141334834658bb526f7
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-28 11:27:52 -0500

    File list clickability.

--- src/visi/filelist.tsx
+++ src/visi/filelist.tsx
@@ -1,32 +1,66 @@
 import React from 'react';
-import { useState, useContext } from 'react';
+import { useState, useContext, createContext } from 'react';
 
 import { sourcefile_list } from './gamedat';
 
 import { ReactCtx } from './context';
 
+export type FileListContextContent = {
+    selected: string;
+    setSelected: (val:string) => void;
+};
+
+function new_context() : FileListContextContent
+{
+    return {
+        selected: '',
+        setSelected: (val) => {},
+    };
+}
+
+const FileListCtx = createContext(new_context());
+
 export function SourceFileList()
 {
+    const [ selected, setSelected ] = useState('');
+    
     let rctx = useContext(ReactCtx);
 
     let ells = sourcefile_list.map(([name, key]) =>
-	<SourceFile filename={ name } key={ key } filekey={ key } />
-    );	
+        <SourceFile filename={ name } key={ key } filekey={ key } />
+    );  
+
+    function evhan_click_background(ev: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        ev.stopPropagation();
+        setSelected('');
+    }
 
     return (
-        <div className="ScrollContent">
-            <ul className="DataList">
-		{ ells }
-            </ul>
-	</div>
+        <FileListCtx.Provider value={ { selected, setSelected } }>
+            <div className="ScrollContent" onClick={ evhan_click_background }>
+                <ul className="DataList">
+                    { ells }
+                </ul>
+            </div>
+        </FileListCtx.Provider>
     );
 }
 
 function SourceFile({ filename, filekey } : { filename:string, filekey:string })
 {
+    let rctx = useContext(ReactCtx);
+    let ctx = useContext(FileListCtx);
+    let selected = ctx.selected;
+
+    function evhan_click(ev: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+        ev.stopPropagation();
+        ctx.setSelected(filekey);
+        rctx.setLoc(filekey+':1:1');
+    }
+    
     return (
-	<li>
-	    { filename }
-	</li>
+        <li className={ (filekey==selected) ? 'Selected' : '' } onClick={ evhan_click }>
+            { filename }
+        </li>
     );
 }
