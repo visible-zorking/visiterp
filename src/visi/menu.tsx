8eaedc9e12dcf5dda3857c766965580d464207da
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-03 10:36:50 -0500

    Menu functionality.

--- src/visi/menu.tsx
+++ src/visi/menu.tsx
@@ -4,29 +4,44 @@ import { useState, useContext, useEffect } from 'react';
 export function AppMenu()
 {
     const [ menuopen, setMenuOpen ] = useState(false);
+    const [ arrangement, setArrangement ] = useState('12');
 
     function handle_click_menu(ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-	ev.stopPropagation();
-	setMenuOpen(!menuopen);
+        ev.stopPropagation();
+        setMenuOpen(!menuopen);
     }
     
-    function handle_click_arrange(ev: React.MouseEvent<HTMLButtonElement, MouseEvent>, key: string) {
-	ev.stopPropagation();
-	document.body.className = 'Arrange'+key;
-	setMenuOpen(false);
+    function handle_click_arrange(val: string) {
+        setArrangement(val);
+        document.body.className = 'Arrange'+val;
+        setMenuOpen(false);
     }
     
     return (
-	<>
-	    <button id="menubutton" onClick={ handle_click_menu }>Menu</button>
-	    <div className={ menuopen ? 'Menu MenuOpen' : 'Menu' }>
-		<div>
-		    <button onClick={ (ev)=>handle_click_arrange(ev, '12') }>A</button>
-		    <button onClick={ (ev)=>handle_click_arrange(ev, '21') }>B</button>
-		    <button onClick={ (ev)=>handle_click_arrange(ev, '121') }>C</button>
-		    <button onClick={ (ev)=>handle_click_arrange(ev, '111') }>D</button>
-		</div>
-	    </div>
-	</>
+        <>
+            <button id="menubutton" onClick={ handle_click_menu }>Menu</button>
+            <div className={ menuopen ? 'Menu MenuOpen' : 'Menu' }>
+                <div>
+                    <ArrangeButton arrange='12' curarrange={ arrangement} handle={ handle_click_arrange } />
+                    <ArrangeButton arrange='21' curarrange={ arrangement} handle={ handle_click_arrange } />
+                    <ArrangeButton arrange='121' curarrange={ arrangement} handle={ handle_click_arrange } />
+                    <ArrangeButton arrange='111' curarrange={ arrangement} handle={ handle_click_arrange } />
+                </div>
+            </div>
+        </>
+    );
+}
+
+function ArrangeButton({ arrange, curarrange, handle }: { arrange:string, curarrange:string, handle:(key:string)=>void })
+{
+    let issel = (arrange == curarrange);
+    
+    function handle_click_arrange(ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        ev.stopPropagation();
+        handle(arrange);
+    }
+    
+    return (
+        <button className={ issel ? 'Selected' : '' } onClick={ handle_click_arrange }>{ arrange }</button>
     );
 }
