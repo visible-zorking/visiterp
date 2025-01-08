6c94de9110ee454d64d7f3994fd83ee9228310b7
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-08 13:35:06 -0500

    Annoying jquery animation logic.

--- src/glkio/commentary.js
+++ src/glkio/commentary.js
@@ -2,11 +2,85 @@
 
 var CommentaryClass = function() {
 
+    let visible = false; // or will be when the current animation completes
+    let animating = false;
+
+    function show(text)
+    {
+        openpane();
+    }
+
+    function openpane()
+    {
+        let panel = $('#commentarypane');
+        if (!panel.length)
+            return;
+
+        if (visible) {
+            return;
+        }
+
+        panel.animate({
+            opacity: 1
+        }, {
+            duration: 1300,
+            start: function() {
+                visible = true;
+                animating = true;
+                panel.show();
+            },
+            step: function(now, fx) {
+                let xval = Math.floor(400-now*400);
+                panel.css('transform', 'translateX('+xval+'px)');
+            },
+            complete: function() {
+                panel.css('transform', '');
+                animating = false;
+            },
+        })
+    }
+    
+    function closepane()
+    {
+        let panel = $('#commentarypane');
+        if (!panel.length)
+            return;
+        
+        if (!visible) {
+            return;
+        }
+
+        panel.animate({
+            opacity: 0
+        }, {
+            duration: 1300,
+            start: function() {
+                visible = false;
+                animating = true;
+            },
+            step: function(now, fx) {
+                let xval = Math.floor(400-now*400);
+                panel.css('transform', 'translateX('+xval+'px)');
+            },
+            complete: function() {
+                panel.hide();
+                panel.css('transform', '');
+                animating = false;
+            },
+        })
+    }
+    
     $('#commentaryclose').on('click', (ev) => {
         ev.preventDefault();
         ev.stopPropagation();
 
-        console.log('### close');
+        closepane();
     });
+
+    return {
+        _classname: 'Commentary',
+        show: show,
+        hide: closepane,
+    };
 }
 
