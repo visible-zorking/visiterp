5c5d78817d4157b78b22987ab637ce0338a2d87e
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-13 13:14:30 -0500

    Timer list buttons. Factored out a TimerEntry widget.

--- src/visi/timers.tsx
+++ src/visi/timers.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-import { gamedat_global_names, gamedat_routine_addrs, unpack_address, signed_zvalue } from './gamedat';
+import { gamedat_global_names, gamedat_routine_addrs, unpack_address, signed_zvalue, check_commentary } from './gamedat';
 
 import { ReactCtx } from './context';
 import { ObjPageLink, Commentary } from './widgets';
+
+export type TimerListContextContent = {
+    selected: number;
+    setSelected: (val:number) => void;
+};
+
+function new_context() : TimerListContextContent
+{
+    return {
+        selected: -1,
+        setSelected: (val) => {},
+    };
+}
+
+const TimerListCtx = createContext(new_context());
 
 export function TimerTable()
 {
@@ -27,32 +42,12 @@ export function TimerTable()
         let flag = zstate.timertable[pos] * 0x100 + zstate.timertable[pos+1];
         let count = zstate.timertable[pos+2] * 0x100 + zstate.timertable[pos+3];
         let addr = zstate.timertable[pos+4] * 0x100 + zstate.timertable[pos+5];
-        let rtn = gamedat_routine_addrs.get(unpack_address(addr));
 
         if (flag)
             activecount++;
 
-        function evhan_click(ev: React.MouseEvent<HTMLLIElement, MouseEvent>) {
-            ev.stopPropagation();
-            setSelected(pos);
-            if (rtn)
-                rctx.setLoc(rtn.sourceloc, false);
-        }
-    
         ells.push(
-            <li key={ pos } className={ (pos==selected) ? 'Selected' : '' } onClick={ evhan_click }>
-                { (flag ?
-                   <span className="TimerActive">&#x2611;</span> :
-                   <span className="TimerInactive">&#x2610;</span>) }
-                {' '}
-                { (rctx.shownumbers ?
-                   <span className="ShowAddr">({ addr }) </span>
-                   : null) }
-                <code>{ rtn ? rtn.name : '???' }</code>
-                {', '}
-                <i>count </i>
-                { signed_zvalue(count) }
-            </li>
+            <TimerEntry key={ pos } addr={ addr } flag={ flag } count={ count } />
         );
         
         timerpos += 6;
@@ -64,14 +59,58 @@ export function TimerTable()
     }
 
     return (
-        <div className="ScrollContent" onClick={ evhan_click_background }>
-            <Commentary topic={ 'TIMERS-LEGEND' } />
+        <TimerListCtx.Provider value={ { selected, setSelected } }>
+            <div className="ScrollContent" onClick={ evhan_click_background }>
+                <Commentary topic={ 'TIMERS-LEGEND' } />
                 <div>
                     { ells.length } timers, { activecount } active:
                 </div>
-            <ul className="DataList">
-                { ells }
-            </ul>
-        </div>
+                <ul className="DataList">
+                    { ells }
+                </ul>
+            </div>
+        </TimerListCtx.Provider>
+    );
+}
+
+function TimerEntry({ addr, flag, count }: { addr:number, flag:number, count:number })
+{
+    let rctx = useContext(ReactCtx);
+    let zstate = rctx.zstate;
+
+    let ctx = useContext(TimerListCtx);
+    let selected = ctx.selected;
+
+    let rtn = gamedat_routine_addrs.get(unpack_address(addr));
+    
+    let withcom: string|undefined;
+    if (rtn) {
+        withcom = check_commentary(rtn.name, 'RTN');
+    }
+
+    function evhan_click(ev: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+        ev.stopPropagation();
+        ctx.setSelected(addr);
+        if (rtn)
+            rctx.setLoc(rtn.sourceloc, false);
+    }
+    
+    return (
+        <li className={ (addr==selected) ? 'Selected' : '' } onClick={ evhan_click }>
+            { (withcom ?
+               <Commentary topic={ withcom } smaller={ true } />
+               : null) }
+            { (flag ?
+               <span className="TimerActive">&#x2611;</span> :
+               <span className="TimerInactive">&#x2610;</span>) }
+            {' '}
+            { (rctx.shownumbers ?
+               <span className="ShowAddr">({ addr }) </span>
+               : null) }
+            <code>{ rtn ? rtn.name : '???' }</code>
+            {', '}
+            <i>count </i>
+            { signed_zvalue(count) }
+        </li>
     );
 }
