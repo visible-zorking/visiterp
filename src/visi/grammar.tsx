aa05322d05685e3cebc7a2b35694c5bca1c68184
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-23 00:16:57 -0500

    Crudest possible grammar table display.

--- src/visi/grammar.tsx
+++ src/visi/grammar.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-import { GrammarLineData } from '../custom/gamedat';
+import { GrammarLineData, gamedat_grammar_lines, gamedat_actions } from '../custom/gamedat';
 
 import { ReactCtx } from './context';
 
 export function GrammarTable()
 {
-    return <p>GRAMMAR</p>;
+    let counter = 0;
+    let glinels = [];
+
+    for (let gline of gamedat_grammar_lines) {
+        let action = gamedat_actions[gline.action];
+        glinels.push(
+            <li>{ gline.text } - { action.name }</li>
+        );
+        counter++;
+    }
+    
+    return (
+        <div className="ScrollContent">
+            <ul className="DataList">
+                { glinels }
+            </ul>
+        </div>
+    );
 }
