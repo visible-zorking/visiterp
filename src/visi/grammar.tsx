b89029522c1846891cc329b7850ffe1310365840
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-23 00:07:17 -0500

    Stub grammar table.

---
+++ src/visi/grammar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState, useContext, createContext } from 'react';
+
+import { GrammarLineData } from '../custom/gamedat';
+
+import { ReactCtx } from './context';
+
+export function GrammarTable()
+{
+    return <p>GRAMMAR</p>;
+}
