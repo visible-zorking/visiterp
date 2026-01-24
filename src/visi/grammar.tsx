b17b8cd2d8bce329e04d5afd426b94a159fda147
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-23 20:56:36 -0500

    Working on the grammar shower.

--- src/visi/grammar.tsx
+++ src/visi/grammar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-import { GrammarLineData, gamedat_grammar_lines, gamedat_actions } from '../custom/gamedat';
+import { GrammarLineData, RoutineData, gamedat_grammar_lines, gamedat_actions, gamedat_routine_addrs } from '../custom/gamedat';
 
 import { ReactCtx } from './context';
 
@@ -11,9 +11,8 @@ export function GrammarTable()
     let glinels = [];
 
     for (let gline of gamedat_grammar_lines) {
-        let action = gamedat_actions[gline.action];
         glinels.push(
-            <li>{ gline.text } - { action.name }</li>
+            <GrammarLine key={ counter } gline={ gline } />
         );
         counter++;
     }
@@ -24,5 +23,50 @@ export function GrammarTable()
                 { glinels }
             </ul>
         </div>
+    );
+}
+
+export function GrammarLine({ gline }: { gline:GrammarLineData })
+{
+    let rctx = useContext(ReactCtx);
+
+    let action = gamedat_actions[gline.action];
+
+    function evhan_click(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>, func: RoutineData) {
+        ev.preventDefault();
+        rctx.setLoc(func.sourceloc, false);
+    }
+    
+    let funcel: JSX.Element|null = null;
+    let prefuncel: JSX.Element|null = null;
+    if (action.acrtn) {
+        let func = gamedat_routine_addrs.get(action.acrtn);
+        if (!func) {
+            funcel = <i>???</i>
+        }
+        else {
+            funcel = (
+                <code><a className="Src_Id" href="#" onClick={ (ev) => evhan_click(ev, func) }>{ func.name }</a></code>
+            );
+        }
+    }
+    if (action.preacrtn) {
+        let func = gamedat_routine_addrs.get(action.preacrtn);
+        if (!func) {
+            prefuncel = <i>???</i>
+        }
+        else {
+            prefuncel = (
+                <code><a className="Src_Id" href="#" onClick={ (ev) => evhan_click(ev, func) }>{ func.name }</a></code>
+            );
+        }
+    }
+    
+    return (
+        <li>
+            { gline.text }
+            {' '}{ prefuncel }
+            {' '}{ funcel }
+        </li>
     );
 }
