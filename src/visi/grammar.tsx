ddf18e0837295ef2b0135e348ca8d45e33e8bb5f
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-23 22:21:28 -0500

    Working on clause display.

--- src/visi/grammar.tsx
+++ src/visi/grammar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-import { GrammarLineData, RoutineData, gamedat_grammar_lines, gamedat_actions, gamedat_routine_addrs } from '../custom/gamedat';
+import { GrammarLineData, RoutineData, gamedat_grammar_lines, gamedat_grammar_verbnums, gamedat_actions, gamedat_routine_addrs } from '../custom/gamedat';
 
 import { ReactCtx } from './context';
 
@@ -30,12 +30,42 @@ export function GrammarLine({ gline }: { gline:GrammarLineData })
 {
     let rctx = useContext(ReactCtx);
 
+    let verb = gamedat_grammar_verbnums.get(gline.num);
+    if (!verb) {
+        return (<i>unknown verb: { gline.num }</i>);
+    }
+    
     let action = gamedat_actions[gline.action];
 
     function evhan_click(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>, func: RoutineData) {
         ev.preventDefault();
         rctx.setLoc(func.sourceloc, false);
     }
+
+    let clausels = gline.clauses.map(clause => {
+        let prepel: JSX.Element|null = null;
+        let attrel: JSX.Element|null = null;
+        if (clause.prep) {
+            prepel = (
+                <>
+                    {' '}
+                    <span className="PrintDictWord">&#x2018;{ clause.prep }&#x2019;</span>
+                </>
+            );
+        }
+        if (clause.attr) {
+            attrel = (
+                <>
+                    :<code>{ clause.attr }</code>
+                </>
+            );
+        }
+        return (
+            <>
+                { prepel } <i>obj</i>{ attrel }
+            </>
+        );
+    });
     
     let funcel: JSX.Element|null = null;
     let prefuncel: JSX.Element|null = null;
@@ -64,7 +94,8 @@ export function GrammarLine({ gline }: { gline:GrammarLineData })
     
     return (
         <li>
-            { gline.text }
+            <span className="PrintDictWord">&#x2018;{ verb.words[0] }&#x2019;</span>
+            {' '}{ clausels }
             {' '}{ prefuncel }
             {' '}{ funcel }
         </li>
