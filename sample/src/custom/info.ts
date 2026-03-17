1800c755085a7bbe02d00071f3ecc022a54f3cd1
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-16 21:28:15 -0400

    Shift sample gamedat.

---
+++ sample/src/custom/info.ts
@@ -0,0 +1,12 @@
+
+/* Return the initial sourceloc to display. */
+export function sourceloc_start() : string
+{
+    return 'J:78:1:101:0';  // 'gverbs.zil', lines 78-100
+}
+
+// Presentation order. Filenames must match game-info!
+export const sourcefile_presentation_list: string[] = [
+    'game.zil',
+    //...more files...
+];
